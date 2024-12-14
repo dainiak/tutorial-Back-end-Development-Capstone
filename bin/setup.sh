1b144f900ea293f19b69afc9b1aa1ca4611b4ae7
Alex Dainiak <alex.dainiak@yandex.ru>
2024-12-14 18:53:13 +0400

    add views

--- bin/setup.sh
+++ bin/setup.sh
@@ -20,8 +20,8 @@ echo 'export PS1="\[\e]0;\u:\W\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32
 echo "source ~/venv/bin/activate" >> ~/.bashrc
 
 echo "Installing Python dependencies..."
-source ~/venv/bin/activate && python3.9 -m pip install --upgrade pip wheel
-source ~/venv/bin/activate && python3.9 -m pip install -r requirements.txt
+source ~/venv/bin/activate && python3 -m pip install --upgrade pip wheel
+source ~/venv/bin/activate && python3 -m pip install -r requirements.txt
 
 echo "Starting the Postgres Docker container..."
 make db
